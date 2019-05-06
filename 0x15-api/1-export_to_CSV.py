ea11a554dad64f245c5f938512a19b059c4d63bc
Wescott Sharples <wesc@ttsharpl.es>
2019-05-06 23:54:34 +0000

    Fix pep8 style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -33,7 +33,6 @@ def print_employee_data(name, tasks):
             complete_tasks += 1
             task_titles.append(task.get('title'))
         total_tasks += 1
-
     print("Employee {} is done with tasks({}/{}):".format(
         name,
         complete_tasks,
@@ -42,25 +41,29 @@ def print_employee_data(name, tasks):
         print('\t {}'.format(title))
 
 
-def export_to_csv(e_id, name, tasks):
-    filename = sys.argv[1] + '.csv'
+def export_to_csv(filename, e_id, name, tasks):
+    filename = filename + '.csv'
     with open(filename, 'w') as f:
         fieldnames = [
             'e_id',
             'name',
             'completed',
             'title']
-        writer = csv.DictWriter(f, quoting=csv.QUOTE_ALL, fieldnames=fieldnames)
+        writer = csv.DictWriter(
+            f,
+            quoting=csv.QUOTE_ALL,
+            fieldnames=fieldnames)
         for task in tasks:
-            writer.writerow({
-                'title': task.get('title'),
-                'completed': task.get('completed'),
-                'name': name,
-                'e_id': e_id})
+            if task.get('userId') == e_id:
+                writer.writerow({
+                    'title': task.get('title'),
+                    'completed': task.get('completed'),
+                    'name': name,
+                    'e_id': e_id})
 
 
 if __name__ == '__main__':
     employee_id = int(sys.argv[1])
     employee_name = get_employee_name(employee_id)
     employee_tasks = get_employee_tasks(employee_id)
-    export_to_csv(employee_id, employee_name, employee_tasks)
+    export_to_csv(sys.argv[1], employee_id, employee_name, employee_tasks)

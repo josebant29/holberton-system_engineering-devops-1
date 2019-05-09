bbd138725e0b09b13bb7cbe50c11afd5859fd350
Wescott Sharples <wesc@ttsharpl.es>
2019-05-09 14:57:21 -0700

    Script prints top posts on subreddit using RedditAPI

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Returns info for a given employee using API.
+"""
+
+import requests
+import sys
+
+
+def top_ten(subreddit):
+
+    headers = {'User-agent': 'Wescott'}
+
+    base = 'https://www.reddit.com/'
+    query = '/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(base + query, headers=headers)
+    response.raise_for_status()
+    top = response.json().get('data', {}).get('children', [])
+    if not top:
+        print(None)
+    for post in top:
+        print(post.get('data').get('title'))
+
+
+if __name__ == '__main__':
+    top_ten('showerthoughts')

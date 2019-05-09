af83d3fdea77df2ea2d3eb1e3611d464038fe2cc
Wescott Sharples <wesc@ttsharpl.es>
2019-05-09 14:57:00 -0700

    Script queries subreddit using RedditAPI

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Returns info for a given employee using API.
+"""
+
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+
+    headers = {'User-agent': 'Wescott'}
+
+    base = 'https://www.reddit.com/'
+    query = '/r/{}/about.json'.format(subreddit)
+    response = requests.get(base + query, headers=headers)
+    response.raise_for_status()
+    subs = response.json().get('data', {}).get('subscribers')
+    if not subs:
+        return 0
+    return subs
+
+
+if __name__ == '__main__':
+    print(number_of_subscribers('showerthoughts'))

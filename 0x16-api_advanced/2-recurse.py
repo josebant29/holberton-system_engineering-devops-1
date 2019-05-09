51088e9b040b9a77fc2cb78373d5f1cf89582a1f
wescottsharples <wesc@ttsharpl.es>
2019-05-09 23:29:26 +0000

    Returns all hot posts of given subreddit recursively

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Returns info for a given employee using API.
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Prints the top ten 'hot' posts for a subreddit.
+    """
+
+    headers = {'User-agent': 'Wescott'}
+
+    base = 'https://www.reddit.com/'
+    query = '/r/{}/hot.json?after={}'.format(subreddit, after)
+    response = requests.get(base + query, headers=headers)
+    top = response.json().get('data', {}).get('children', [])
+    after = response.json().get('data', {}).get('after')
+    if not after:
+        return hot_list
+    for post in top:
+        hot_list.append(post.get('data').get('title'))
+    return recurse(subreddit, hot_list=hot_list, after=after)

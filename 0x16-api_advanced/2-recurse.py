b4350814829b3e470f85e68d7f13a8b61cf86e2c
Wescott Sharples <wesc@ttsharpl.es>
2019-05-10 05:17:41 +0000

    Add check for None

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -18,10 +18,10 @@ def recurse(subreddit, hot_list=[], after=None):
     response = requests.get(base + query, headers=headers)
     top = response.json().get('data', {}).get('children', [])
     after = response.json().get('data', {}).get('after', None)
-    if response != 200:
-        return None
-    if not after:
+    if not top:
         return hot_list
     for post in top:
         hot_list.append(post.get('data').get('title'))
-    return recurse(subreddit, hot_list=hot_list, after=after)
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list, after)

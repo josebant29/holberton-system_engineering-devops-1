85c68ca71bd67b05ab4066af5bf62af42274b199
Wescott Sharples <wesc@ttsharpl.es>
2019-05-10 05:07:23 +0000

    Remove main

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,19 +13,13 @@ def recurse(subreddit, hot_list=[], after=None):
 
     headers = {'User-agent': 'Wescott'}
 
-    try:
-        base = 'https://www.reddit.com/'
-        query = '/r/{}/hot.json?after={}'.format(subreddit, after)
-        response = requests.get(base + query, headers=headers)
-        top = response.json().get('data', {}).get('children', [])
-        after = response.json().get('data', {}).get('after', None)
-        if not after:
-            return hot_list
-        for post in top:
-            hot_list.append(post.get('data').get('title'))
-        return recurse(subreddit, hot_list=hot_list, after=after)
-    except:
-        return None
-
-if __name__ == '__main__':
-    print(recurse('programming'))
+    base = 'https://www.reddit.com/'
+    query = '/r/{}/hot.json?after={}'.format(subreddit, after)
+    response = requests.get(base + query, headers=headers)
+    top = response.json().get('data', {}).get('children', [])
+    after = response.json().get('data', {}).get('after', None)
+    if not after:
+        return hot_list
+    for post in top:
+        hot_list.append(post.get('data').get('title'))
+    return recurse(subreddit, hot_list=hot_list, after=after)

939a202d4d9525ea47f29dfc1051cb0269122f42
Wescott Sharples <wescottsharples@Wescotts-MacBook-Pro.local>
2019-05-30 20:38:51 -0700

    Fix alignment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Change Nginx open file limit
 exec { 'update':
   command => 'sed -i "s/15/2000/" /etc/default/nginx && /etc/init.d/nginx restart',
-  path  => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games'
 }
